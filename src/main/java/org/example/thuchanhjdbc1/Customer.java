6bc88cdd658c694203869499bedda53dbc6acfe9
LocTM <tranminhloc.anhk@gmail.com>
2024-02-16 23:37:50 +0700

    add getter/setter

--- src/main/java/org/example/thuchanhjdbc1/Customer.java
+++ src/main/java/org/example/thuchanhjdbc1/Customer.java
@@ -14,6 +14,27 @@ public class Customer {
         this.address = address;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
