06a89d33fd524d7207504e1e9df8fa980ce7aebd
LocTM <tranminhloc.anhk@gmail.com>
2024-02-16 23:37:17 +0700

    add Contructor

--- src/main/java/org/example/thuchanhjdbc1/Customer.java
+++ src/main/java/org/example/thuchanhjdbc1/Customer.java
@@ -5,4 +5,15 @@ public class Customer {
     private String dateOfBirth;
     private String address;
 
+    public Customer() {
+    }
+
+    public Customer(String name, String dateOfBirth, String address) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+
+
 }
